22c9d0802dfc7c2a2a340c89edf6b06440add763
Chandan K M <chandanchintu16@gmail.com>
2026-05-26 21:50:25 +0530

    find + count + delete + update using put and patch APIs

--- src/main/java/com/jpaandhibernate/example/hospital_management/controller/DoctorController.java
+++ src/main/java/com/jpaandhibernate/example/hospital_management/controller/DoctorController.java
@@ -3,10 +3,10 @@ package com.jpaandhibernate.example.hospital_management.controller;
 import com.jpaandhibernate.example.hospital_management.model.Doctor;
 import com.jpaandhibernate.example.hospital_management.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.Doc;
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctor/apis")
@@ -24,6 +24,44 @@ public class DoctorController {
     @PostMapping("/save")
     public String saveDoctor(@RequestBody Doctor doctor){
         String response = doctorService.saveDoctor(doctor);
+        return response;
+    }
+
+    @GetMapping("/findAll")
+    public List<Doctor> findAllDoctors(){
+        List<Doctor> doctorList = doctorService.getAllDoctors();
+        return doctorList;
+    }
+
+    //@PathVariable - it is used to take the inputs in input request url path
+    @GetMapping("/findById/{id}")
+    public Doctor findDoctorById(@PathVariable int id){
+        Doctor doctor = doctorService.getDoctorById(id);
+        return doctor;
+    }
+
+    @GetMapping("/count")
+    public String countDoctors(){
+        String response  = doctorService.countDoctors();
+        return response;
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public String deleteDoctorsById(@PathVariable int id){
+        String response = doctorService.deleteDoctorById(id);
+        return response;
+    }
+
+    @PutMapping("/updatePut/{id}")
+    public String updateDoctorUsingPut(@PathVariable int id,@RequestBody Doctor doctor){
+        String response = doctorService.updateDoctorUsingPut(id,doctor);
+        return response;
+    }
+
+    // @RequestParam - it takes the input in the form of parameter query
+    @PatchMapping("/updatePatch/{id}")
+    public String updateDoctorUsingPatch(@PathVariable int id,@RequestParam String newEmail,@RequestParam String newMobile){
+        String response  = doctorService.updateDoctorUsingPatch(id, newEmail, newMobile);
         return response;
     }
 }
--- src/main/java/com/jpaandhibernate/example/hospital_management/service/DoctorService.java
+++ src/main/java/com/jpaandhibernate/example/hospital_management/service/DoctorService.java
@@ -5,6 +5,9 @@ import com.jpaandhibernate.example.hospital_management.repository.DoctorReposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service // it contains business logic
 public class DoctorService {
 
@@ -14,5 +17,63 @@ public class DoctorService {
     public String saveDoctor(Doctor doctor){
         doctorRepository.save(doctor);
         return "Doctor saved successfully!!";
+    }
+
+    public List<Doctor> getAllDoctors(){
+        List<Doctor> doctorList = doctorRepository.findAll();
+        return doctorList;
+    }
+
+    public Doctor getDoctorById(int id){
+        Optional<Doctor> doctorOptional = doctorRepository.findById(id);
+        if(doctorOptional.isPresent()){
+            return doctorOptional.get();
+        } else {
+            return null;
+        }
+    }
+    
+    public String countDoctors(){
+        long totalCount = doctorRepository.count();
+        return "Doctors present are : "+totalCount;
+    }
+
+    public String deleteDoctorById(int id){
+        doctorRepository.deleteById(id);
+        return "Doctor with id : "+id+" deleted successfully";
+    }
+
+    //update using put operation - updates complete object
+    public String updateDoctorUsingPut(int id, Doctor newDoctorRequest){
+        // find doctor with id
+        // if doctor is present, update it
+        // else we cannot update
+        Doctor existingDoctor = getDoctorById(id);
+        if(existingDoctor!=null){
+            // proceed to update
+            doctorRepository.save(newDoctorRequest);
+            return "Doctor with id : "+id+" updated successfully";
+        } else {
+            // cannot update
+            return "Doctor is not present with id : "+id+", hence cannot update";
+        }
+    }
+
+    //update using patch operation - single specific fields
+    public String updateDoctorUsingPatch(int id, String newEmail, String newMobile){
+        // find doctor with id
+        // if doctor is present, update it
+        // else we cannot update
+        Doctor existingDoctor = getDoctorById(id);
+        if(existingDoctor!=null){
+            // proceed to update
+            existingDoctor.setMobile(newMobile);
+            existingDoctor.setEmail(newEmail);
+            doctorRepository.save(existingDoctor);
+            return "Doctor with id : "+id+" updated successfully";
+        } else {
+            // cannot update
+            return "Doctor is not present with id : "+id+", hence cannot update";
+        }
     }
 }
